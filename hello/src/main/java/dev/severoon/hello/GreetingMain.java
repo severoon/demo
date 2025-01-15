9fe242167987a34ea844d7de26187d5c38ce2aae
Steven Rose <severoon@users.noreply.github.com>
2025-01-15 20:30:38 +0000

    Replace System.err with constant in GreetingMain.

--- hello/src/main/java/dev/severoon/hello/GreetingMain.java
+++ hello/src/main/java/dev/severoon/hello/GreetingMain.java
@@ -5,10 +5,13 @@ import static dev.severoon.hello.module.GreetingMainModule.greetingMainModuleFor
 import static java.lang.String.format;
 
 import dev.severoon.hello.app.GreetingApp;
+import java.io.PrintWriter;
 import java.util.Optional;
 
 /** Main class to configure and run a {@link GreetingApp}. */
 public final class GreetingMain {
+
+  private static final PrintWriter ERR = new PrintWriter(System.err);
 
   /** Exit code of {@link GreetingMain}. */
   private enum ExitCode {
@@ -24,8 +27,8 @@ public final class GreetingMain {
   /** Parses specified command line {@code args} into a {@link UserConfig} record. */
   private static UserConfig parseConfig(String[] args) {
     if (args.length > 1) {
-      System.err.println(format("Usage: %s [greeting]", GreetingMain.class.getSimpleName()));
-      System.err.println(
+      ERR.println(format("Usage: %s [greeting]", GreetingMain.class.getSimpleName()));
+      ERR.println(
           format("Example: java %s \"Hello, Guice world!\"", GreetingMain.class.getSimpleName()));
       System.exit(ExitCode.INVALID_ARGUMENTS.ordinal());
     }
